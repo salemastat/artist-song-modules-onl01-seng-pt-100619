1ddd94a53d5233734d976895b2d2a975f4d3a128
Salem Asrat <salemasrat@gmail.com>
2019-11-21 17:39:10 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,4 +2,12 @@ module Memorable
   module ClassMethods
     def reset_all
       self.all.clear
-end
+  end
+  
+ module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end   
+
